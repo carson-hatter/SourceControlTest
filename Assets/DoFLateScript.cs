7e3bcaf5aa45b85ad69a4086c51ce66d85efd8ef
carson <carson.hatter@yahoo.com>
2015-03-04 22:53:28 -0600

    "Dynamic" depth of field, a rough draft

---
+++ Assets/DoFLateScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+using System.Collections;
+
+public class DoFLateScript : MonoBehaviour {
+
+	private DepthOfField dof;
+
+	// Use this for initialization
+	void Start () {
+		dof = GetComponent < DepthOfField> ();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		RaycastHit hit;
+		Ray downRay = new Ray(transform.position, -Vector3.up);
+		if (Physics.Raycast(downRay, out hit)) {
+			dof.focalLength = (hit.distance > 100) ? 100 : hit.distance;
+		}
+	}
+}
